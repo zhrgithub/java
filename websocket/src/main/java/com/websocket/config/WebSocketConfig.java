fdcbbf0b097a5d6ea04279418aa88998da8a6db9
zhrgithub <zhr_java@163.com>
2022-04-27 13:25:16 +0800

    websocket demo

---
+++ websocket/src/main/java/com/websocket/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ *
+ * @author zhanghongrui
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
+
