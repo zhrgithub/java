fe344b9280050a3fe317ef685c888bfc4a0f7bc0
zhrgithub <zhr_java@163.com>
2022-04-27 16:09:10 +0800

    spring的aop与两种动态代理方式

---
+++ springStudy/src/main/java/com/springstudy/SpringStudyApplication.java
@@ -0,0 +1,14 @@
+package com.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringStudyApplication.class, args);
+    }
+
+}
