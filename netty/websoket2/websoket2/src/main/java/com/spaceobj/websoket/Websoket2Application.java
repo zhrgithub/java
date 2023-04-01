fb32351d4109b9af63bb48ff58d162648224ecca
zhrgithub <zhr_java@163.com>
2023-04-01 18:30:43 +0800

    websocket2

---
+++ netty/websoket2/websoket2/src/main/java/com/spaceobj/websoket/Websoket2Application.java
@@ -0,0 +1,14 @@
+package com.spaceobj.websoket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websoket2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Websoket2Application.class, args);
+    }
+
+}
